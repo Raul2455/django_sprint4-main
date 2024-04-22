9f3c65c222973dbc12c39fefb12900c07ab21b31
Raul2455 <kirillmendrin245@gmail.com>
2024-04-22 14:41:34 +0300

    Внесены небольшие изменения

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -17,20 +17,17 @@ class RulesView(TemplateView):
 
 def csrf_failure(request, reason='') -> HttpResponse:
     """Рендер страницы для 403 ошибки."""
-    
-    template: str = 'pages/403csrf.html'
+    template = 'pages/403csrf.html'
     return render(request, template, status=403)
 
 
 def page_not_found(request, exception) -> HttpResponse:
     """Рендер страницы для 404 ошибки."""
-    
-    template: str = 'pages/404.html'
+    template = 'pages/404.html'
     return render(request, template, status=404)
 
 
 def server_error(request, reason='') -> HttpResponse:
     """Рендер страницы для 500 ошибки."""
-    
-    template: str = 'pages/500.html'
+    template = 'pages/500.html'
     return render(request, template, status=500)
